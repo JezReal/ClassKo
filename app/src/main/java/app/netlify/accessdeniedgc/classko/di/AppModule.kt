ac411c5a5fc2a73d07c7f1068032db9241d31c18
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-03-18 15:19:48 +0800

    setup app module dependencies

--- app/src/main/java/app/netlify/accessdeniedgc/classko/di/AppModule.kt
+++ app/src/main/java/app/netlify/accessdeniedgc/classko/di/AppModule.kt
@@ -1,5 +1,27 @@
 package app.netlify.accessdeniedgc.classko.di
 
+import app.netlify.accessdeniedgc.classko.network.GoogleCalendarApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://www.googleapis.com/calendar/v3/"
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideGoogleCalendarApi(): GoogleCalendarApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(GoogleCalendarApi::class.java)
+    }
 }
