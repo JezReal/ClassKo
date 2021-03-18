c719bded85979b0bbd9e99e30b2b8e073afbe07a
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-03-19 01:29:10 +0800

    update function annotation and signature

--- app/src/main/java/app/netlify/accessdeniedgc/classko/network/GoogleCalendarApi.kt
+++ app/src/main/java/app/netlify/accessdeniedgc/classko/network/GoogleCalendarApi.kt
@@ -4,13 +4,14 @@ import app.netlify.accessdeniedgc.classko.model.calendar.CalendarList
 import app.netlify.accessdeniedgc.classko.model.event.EventsList
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GoogleCalendarApi {
 
 
-    @GET
-    fun getCalendarList(): Response<List<CalendarList>>
+    @GET("users/me/calendarList")
+    suspend fun getCalendarList(): Response<CalendarList>
 
-    @GET
-    fun getEventList(): Response<List<EventsList>>
+    @GET("calendars/{calendarId}/events")
+    suspend fun getEventList(@Path("calendarId") id: String): Response<List<EventsList>>
 }
