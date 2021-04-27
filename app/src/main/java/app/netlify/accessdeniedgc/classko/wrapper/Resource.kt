0313cbbaab8c88609e55b73149d7098e74f009a5
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-04-27 23:40:37 +0800

    refactor code

--- app/src/main/java/app/netlify/accessdeniedgc/classko/wrapper/Resource.kt
+++ app/src/main/java/app/netlify/accessdeniedgc/classko/wrapper/Resource.kt
@@ -1,6 +1,9 @@
 package app.netlify.accessdeniedgc.classko.wrapper
 
-sealed class Resource<T>(val data: T?, val message: String?) {
+sealed class Resource<T>(
+    val data: T?,
+    val message: String?
+) {
     class Success<T>(data: T) : Resource<T>(data, null)
     class Failure<T>(message: String) : Resource<T>(null, message)
 }
