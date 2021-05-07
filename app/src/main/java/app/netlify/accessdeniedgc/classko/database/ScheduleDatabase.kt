06268469cccf692f73baf87d19c0b1c9f8230b97
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-05-08 00:52:55 +0800

    update database version

--- app/src/main/java/app/netlify/accessdeniedgc/classko/database/ScheduleDatabase.kt
+++ app/src/main/java/app/netlify/accessdeniedgc/classko/database/ScheduleDatabase.kt
@@ -3,7 +3,11 @@ package app.netlify.accessdeniedgc.classko.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Schedule::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Schedule::class],
+    version = 2,
+    exportSchema = false
+)
 abstract class ScheduleDatabase : RoomDatabase() {
 
     abstract val scheduleDao: ScheduleDao
