5bf628a4517b997e061845f96b2f64c3f2418b6d
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-05-31 17:55:18 +0800

    refactor api response model

--- app/src/main/java/app/netlify/accessdeniedgc/classko/network/Schedule.kt
+++ app/src/main/java/app/netlify/accessdeniedgc/classko/network/Schedule.kt
@@ -1,11 +1,12 @@
 package app.netlify.accessdeniedgc.classko.network
 
-
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Schedule(
+    @Json(name = "type")
+    val type: String?,
     @Json(name = "scheduleItems")
     val scheduleItems: List<ScheduleItem>
 )
